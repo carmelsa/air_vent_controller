cd86eabd872ab18b2dd7f41475ffd459d049ce63
matan963210 <matan963210@gmail.com>
2021-08-29 17:36:34 +0300

    updating open hour

--- open_close.py
+++ open_close.py
@@ -1,10 +1,9 @@
 import datetime
 
 
-def open_or_close(vent_status, ):
+def open_or_close(vent_status):
     is_input_valid(vent_status)
     wanted_status = get_wanted_vent_status()
-    get_wanted_vent_status()
     if vent_status == wanted_status:
         print('you dont need to change the status')
     else:
@@ -12,33 +11,35 @@ def open_or_close(vent_status, ):
 
 
 def is_input_valid(situation):
-    if situation == 'open':
-        valid = 0
-        return valid
-    elif situation == 'close':
-        valid = 0
-        return valid
+    if situation == 'open' or situation == 'close':
+        return
     else:
         print('The inserted situation is not accepted')
-        valid = 1
-
+        exit()
 
 
 def get_wanted_vent_status():
     current = datetime.datetime.now().time()
     start = datetime.time(12, 0, 0)
-    end = datetime.time(15, 0, 0)
+    end = datetime.time(16, 0, 0)
     time = start <= current <= end
     if time == True:
         wanted_status = 'close'
         return wanted_status
+    start = datetime.time(16, 0, 0)
+    end = datetime.time(21, 0, 0)
+    time = start <= current <= end
+    if time == True:
+        wanted_status = 'open'
+        return wanted_status
+
     else:
         wanted_status = 'open'
         return wanted_status
 
 
 def main():
-    open_or_close('fgcxggfd')
+    open_or_close('fghffg')
 
 
 if __name__ == '__main__':
