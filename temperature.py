578ccec47508608c6221f496fdc4e48777b180ee
matan963210 <matan963210@gmail.com>
2021-09-01 19:30:15 +0300

    fix shani comments

--- temperature.py
+++ temperature.py
@@ -1,30 +1,41 @@
-temperature = [16, 17, 18, 19, 20]
+list_temperatures = []
+highest_temperature = 40
+lowest_temperature = 16
 
 
-def valid_check(temperature):
-    for i in temperature:
-        if i > 40:
+def get_all():
+    get_avg_temp(list_temperatures)
+    get_min_temp(list_temperatures)
+    get_max_temp(list_temperatures)
+
+
+def valid_check(temperatures):
+    for i in temperatures:
+        if i > highest_temperature:
             print('The temperature entered is not valid')
             exit()
-        if i < 16:
+        if i < lowest_temperature:
             print('The temperature entered is not valid')
             exit()
 
 
-def get_avg_temp(temperature):
-    return sum(temperature) / len(temperature)
+def get_avg_temp(temperatures):
+    return sum(temperatures) / len(temperatures)
 
 
-def get_min_temp(temperature):
-    temperature.sort()
-    return temperature[0]
+def get_min_temp(temperatures):
+    temperatures.sort()
+    return temperatures[0]
 
 
-def get_max_temp(temperature):
-    temperature.sort()
-    return temperature[-1]
+def get_max_temp(temperatures):
+    temperatures.sort()
+    return temperatures[-1]
 
 
-get_avg_temp(temperature)
-get_min_temp(temperature)
-get_max_temp(temperature)
+def main():
+    get_all()
+
+
+if __name__ == '__main__':
+    main()
